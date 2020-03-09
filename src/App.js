4e7e47b25e8b712575b88d65a52c3351d3daf805
Rafal Mroz <rafalmroz30@gmail.com>
2020-03-09 18:26:54 +0100

    Revert "feat: extract Header component to a new file"
    This reverts commit b7a5d7b10e6e89afcc6a76a55bad8537761085e7.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,60 @@
 import React from 'react';
 import './App.scss';
-import Header from './components/Header'
 
+class Header extends React.Component {
+
+state = {
+  active: false
+}  
+  
+toggleClass() {
+  const currentState= this.state.active;
+  this.setState({ active: !currentState});
+}
+
+handleClick = () => {
+  this.toggleClass();
+}
+
+
+  render (onClick) {
+    return <>
+    <header className="header">
+        <div className="header__bar">
+
+          <button 
+          onClick={this.handleClick} 
+          className={this.state.active ? 'hamburger--active': "hamburger"} 
+>
+            <span className="hamburger__box">
+              <span className="hamburger__inner"></span>
+            </span>
+          </button>
+
+          <h1 className="bank-name">Neverland Bank</h1>
+          <button className="button-login">Zaloguj</button>
+        </div>
+
+        <nav 
+        className={this.state.active ?  'navigation--active' :  'navigation' } 
+       
+        
+        >
+          <div class="navigation">
+  <ul class="navigation__list">
+    <li class="navigation__item"><a href="">Konto osobiste</a></li>
+    <li class="navigation__item"><a href="">Kredyt hipoteczny</a></li>
+    <li class="navigation__item"><a href="">Wiedza o finansach</a></li>
+    <li class="navigation__item"><a href="">Aktualności</a></li>
+  </ul>
+</div>
+
+        </nav>
+        <br />
+      </header>
+    </>
+  }
+}
 
 function App() {
   
