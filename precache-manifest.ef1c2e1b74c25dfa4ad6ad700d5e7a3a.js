07c25f84242afe8c4e8c19e5d850a63e077c1910
Rafal Mroz <rafalmroz30@gmail.com>
2020-03-09 18:00:16 +0100

    Updates

---
+++ precache-manifest.ef1c2e1b74c25dfa4ad6ad700d5e7a3a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a397d97242e7192bac81e6a9243b800d",
+    "url": "/mrozrafal/banking-application/index.html"
+  },
+  {
+    "revision": "c780fbb6399b8d34cd6b",
+    "url": "/mrozrafal/banking-application/static/css/main.7e192797.chunk.css"
+  },
+  {
+    "revision": "5b4a700ebe9c71fdbd79",
+    "url": "/mrozrafal/banking-application/static/js/2.14c869f6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mrozrafal/banking-application/static/js/2.14c869f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c780fbb6399b8d34cd6b",
+    "url": "/mrozrafal/banking-application/static/js/main.b549e064.chunk.js"
+  },
+  {
+    "revision": "543af52e4bc687df66d8",
+    "url": "/mrozrafal/banking-application/static/js/runtime-main.40198a73.js"
+  },
+  {
+    "revision": "4909f03724235959db968cca57972251",
+    "url": "/mrozrafal/banking-application/static/media/happy-people-holdings-hands.4909f037.jpg"
+  },
+  {
+    "revision": "c034f4da2c2ec06b826e354210e37a9a",
+    "url": "/mrozrafal/banking-application/static/media/nice-looking-apartment.c034f4da.jpg"
+  },
+  {
+    "revision": "2b86b0a87cb4790ae11abe8b3916f666",
+    "url": "/mrozrafal/banking-application/static/media/woman-holding-smartphone.2b86b0a8.jpg"
+  }
+]);
